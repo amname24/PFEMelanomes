1888336bc3be0fafe2236667442b9f4194ac741d
amname24 <amna.am24@gmail.com>
2019-02-15 22:09:39 +0100

    final android app

--- AndroidApp/PFE/app/src/main/java/com/example/me/pfe/MainActivity.java
+++ AndroidApp/PFE/app/src/main/java/com/example/me/pfe/MainActivity.java
@@ -8,12 +8,16 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -24,6 +28,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -60,9 +65,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		int id = v.getId();
 		switch (id) {
-		case R.id.take_photo:
-			takePhoto();
-			break;
+			case R.id.take_photo:
+				takePhoto();
+				break;
 		}
 	}
 
@@ -91,18 +96,18 @@ public class MainActivity extends Activity implements OnClickListener {
 //			}
 		}
 	}
-	
+
 	private void sendPhoto(Bitmap bitmap) throws Exception {
 		new UploadTask().execute(bitmap);
 	}
 
 	private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
-		
+		private String message;
 		protected Void doInBackground(Bitmap... bitmaps) {
 			if (bitmaps[0] == null)
 				return null;
 			setProgress(0);
-			
+
 			Bitmap bitmap = bitmaps[0];
 			ByteArrayOutputStream stream = new ByteArrayOutputStream();
 			bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // convert Bitmap to ByteArrayOutputStream
@@ -111,7 +116,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			DefaultHttpClient httpclient = new DefaultHttpClient();
 			try {
 				HttpPost httppost = new HttpPost(
-						"http://192.168.0.15:8080/PFEjee/Upload"); // server
+						"http://192.168.1.73:8080/PFEjee/Upload"); // server
 
 				MultipartEntity reqEntity = new MultipartEntity();
 				reqEntity.addPart("myFile",
@@ -122,6 +127,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				HttpResponse response = null;
 				try {
 					response = httpclient.execute(httppost);
+
 				} catch (ClientProtocolException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -130,8 +136,14 @@ public class MainActivity extends Activity implements OnClickListener {
 					e.printStackTrace();
 				}
 				try {
-					if (response != null)
-						Log.i(TAG, "response " + response.getStatusLine().toString());
+					HttpEntity entity = response.getEntity();
+//					System.out.println(EntityUtils.toString(entity));
+					if (entity != null) {
+						this.message = EntityUtils.toString(entity);
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
 				} finally {
 
 				}
@@ -159,18 +171,18 @@ public class MainActivity extends Activity implements OnClickListener {
 
 			return null;
 		}
-		
+
 		@Override
 		protected void onProgressUpdate(Void... values) {
 			// TODO Auto-generated method stub
 			super.onProgressUpdate(values);
 		}
-		
+
 		@Override
 		protected void onPostExecute(Void result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
-			Toast.makeText(MainActivity.this, R.string.uploaded, Toast.LENGTH_LONG).show();
+			Toast.makeText(MainActivity.this, this.message, Toast.LENGTH_LONG).show();
 		}
 	}
 
@@ -192,93 +204,93 @@ public class MainActivity extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		super.onConfigurationChanged(newConfig);
 	}
-	
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
 		super.onSaveInstanceState(outState);
 		Log.i(TAG, "onSaveInstanceState");
 	}
-	
+
 	String mCurrentPhotoPath;
-	
+
 	static final int REQUEST_TAKE_PHOTO = 1;
 	File photoFile = null;
 
 	private void dispatchTakePictureIntent() {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    // Ensure that there's a camera activity to handle the intent
-	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-	        // Create the File where the photo should go
-	        File photoFile = null;
-	        try {
-	            photoFile = createImageFile();
-	        } catch (IOException ex) {
-	            // Error occurred while creating the File
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			try {
+				photoFile = createImageFile();
+			} catch (IOException ex) {
+				// Error occurred while creating the File
 
-	        }
-	        // Continue only if the File was successfully created
-	        if (photoFile != null) {
-	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-	            		Uri.fromFile(photoFile));
-	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-	        }
-	    }
+			}
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+						FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider",photoFile));
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			}
+		}
 	}
 
 	/**
 	 * http://developer.android.com/training/camera/photobasics.html
 	 */
 	private File createImageFile() throws IOException {
-	    // Create an image file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-	    String imageFileName = "JPEG_" + timeStamp + "_";
-	    String storageDir = Environment.getExternalStorageDirectory() + "/picupload";
-	    File dir = new File(storageDir);
-	    if (!dir.exists())
-	    	dir.mkdir();
-	    
-	    File image = new File(storageDir + "/" + imageFileName + ".jpg");
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		String storageDir = Environment.getExternalStorageDirectory() + "/picupload";
+		File dir = new File(storageDir);
+		if (!dir.exists())
+			dir.mkdir();
 
-	    // Save a file: path for use with ACTION_VIEW intents
-	    mCurrentPhotoPath = image.getAbsolutePath();
-	    Log.i(TAG, "photo path = " + mCurrentPhotoPath);
-	    return image;
+		File image = new File(storageDir + "/" + imageFileName + ".jpg");
+
+		// Save a file: path for use with ACTION_VIEW intents
+		mCurrentPhotoPath = image.getAbsolutePath();
+		Log.i(TAG, "photo path = " + mCurrentPhotoPath);
+		return image;
 	}
-	
+
 	private void setPic() {
 		// Get the dimensions of the View
-	    int targetW = mImageView.getWidth();
-	    int targetH = mImageView.getHeight();
+		int targetW = mImageView.getWidth();
+		int targetH = mImageView.getHeight();
 
-	    // Get the dimensions of the bitmap
-	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-	    bmOptions.inJustDecodeBounds = true;
-	    BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-	    int photoW = bmOptions.outWidth;
-	    int photoH = bmOptions.outHeight;
+		// Get the dimensions of the bitmap
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
 
-	    // Determine how much to scale down the image
-	    int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+		// Determine how much to scale down the image
+		int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
 
-	    // Decode the image file into a Bitmap sized to fill the View
-	    bmOptions.inJustDecodeBounds = false;
-	    bmOptions.inSampleSize = scaleFactor << 1;
-	    bmOptions.inPurgeable = true;
+		// Decode the image file into a Bitmap sized to fill the View
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor << 1;
+		bmOptions.inPurgeable = true;
 
-	    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-	    
-	    Matrix mtx = new Matrix();
-	    mtx.postRotate(90);
-	    // Rotating Bitmap
-	    Bitmap rotatedBMP = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mtx, true);
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
 
-	    if (rotatedBMP != bitmap)
-	    	bitmap.recycle();
-	    
-	    mImageView.setImageBitmap(rotatedBMP);
-	    
-	    try {
+		Matrix mtx = new Matrix();
+		mtx.postRotate(90);
+		// Rotating Bitmap
+		Bitmap rotatedBMP = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mtx, true);
+
+		if (rotatedBMP != bitmap)
+			bitmap.recycle();
+
+		mImageView.setImageBitmap(rotatedBMP);
+
+		try {
 			sendPhoto(rotatedBMP);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
